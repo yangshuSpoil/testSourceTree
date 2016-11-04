c85a918f7fb0e5958bc938bd9122782a37b92950
ysh <ysh@macmatoMacBook-Pro.local>
2016-11-04 09:25:37 +0800

    ddddddd

---
+++ fafaddsaf/fafaddsaf/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  fafaddsaf
+//
+//  Created by seven。 on 15/8/4.
+//  Copyright (c) 2015年 seven。. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
